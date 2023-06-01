6e1ae175ec3ff322deee6e8f42af16ba62eb9f3e
prfnvch <101510580+prfnvch@users.noreply.github.com>
2023-06-01 13:54:44 +0300

    feature/read/fixes-1

--- static_files/js/scripts.js
+++ static_files/js/scripts.js
@@ -31,19 +31,3 @@ triggerTabList.forEach(function (triggerEl) {
 })
 
 
-const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
-
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.offsetWidth;
-  slidesContainer.scrollLeft += slideWidth;
-});
-
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.offsetWidth;
-  slidesContainer.scrollLeft -= slideWidth;
-});
-
-
